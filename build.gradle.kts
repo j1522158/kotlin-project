bf86621686989fd50790b09148f5829b5447c7dd
j1522158 <j1522158vu@gmail.com>
2024-06-16 23:10:25 +0900

    [update]依存関係の追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.2.6"
 	id("io.spring.dependency-management") version "1.1.5"
+	// KotlinコードをJVMバイトコードへコンパイルするために必要
 	kotlin("jvm") version "1.9.24"
+	// Springの機能をKotlinに統合するために必要
 	kotlin("plugin.spring") version "1.9.24"
 }
 
@@ -35,6 +37,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	// Swagger SpringFoxはSpring3系には未対応
@@ -45,6 +48,14 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 	// Arrow
 	implementation("io.arrow-kt:arrow-core")
+
+	// KotlinクラスをJSONにシリアライズ/デシリアライズするためのサポート機能
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	// KotsetのJUnit5ランナー
+	testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.4.2")
+	testImplementation("io.kotest:kotest-property:5.4.2")
 }
 
 tasks.withType<KotlinCompile> {
